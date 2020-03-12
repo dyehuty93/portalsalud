ea8bb83b2372b071f76c9b2ae9e1f599db80ca9d
dyehuty93 <62066293+dyehuty93@users.noreply.github.com>
2020-03-11 19:54:43 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+$DesdeNumero = 100;
+$HastaNumero = 1000;
+$numeroAleatorio = rand($DesdeNumero, $HastaNumero);
+
+$nick = $_REQUEST["nick"];
+$canal = $_REQUEST["canal"];
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="es-ES"> 
+<head> 
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <link rel="shortcut icon" href="imagenes/fa.png" />
+    <link rel="stylesheet" type="text/css" href="plantilla.css" />
+    <title>webchat</title>
+</head>
+<body>
+<div id="box-1">
+	<table width="100%" >
+  <tr >
+    <td width="20%" valgn="top">
+    	<h1><center><b>ANUNCIO</b></center></h1><br>
+    	<br>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Nuevo 2020 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2454502109148720"
+     data-ad-slot="1227560183"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+ 
+  <br>
+  </td>
+ 
+    <td  width="80%" height="100%" >
+
+<!-- Inicio del Chat Kiwis -->
+<iframe src="https://kiwiirc.com/client/irc.kiwiirc.com/?&theme=cli#webcam" style="border:0; width:100%; height:450px;"></iframe>
+ </td>
+</tr>
+
+</table>
+</div> 
+</body>
+</html>
